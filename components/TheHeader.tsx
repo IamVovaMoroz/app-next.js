023be52e48cc43bba8a09b8c8413cab598873263
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-09-08 17:30:13 +0200

    ok

--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 import styles from "./TheHeader.module.css"; // Импортируем стили
 
 const TheHeader = () => {
-  return (
-    <footer className={styles.container}> {/* Применяем стили */}
-      <Link href="/">Home</Link>
-      <Link href="/blog">Blog</Link>
-      <Link href="/about">About</Link>
-    </footer>
-  );
+    return (
+        <header className={styles.container}> {/* Применяем стили */}
+            <Link href="/">Home</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/about">About</Link>
+        </header>
+    );
 };
 
 export { TheHeader };
