3c32cdec2aa56146cffca386bb2580d52d992ec3
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-09-12 20:06:27 +0200

    ok

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,80 +1,119 @@
-// Импортируем необходимые модули и компоненты
-"use client"; // Используется для всех клиентских компонентов
-import styles from "./Navigation.module.css"; // Импортируем стили
+"use client";
+// Navigation.tsx
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import {useRouter} from "next/router"
+import styles from "./Navigation.module.css"; // Импортируем стили
 
-// Определяем тип для элемента навигации
 type NavLink = {
   label: string;
   href: string;
 };
-
-// Определяем тип для пропсов компонента
 type Props = {
   navLinks: NavLink[];
 };
 
-// Определяем компонент навигации
-// const Navigation = ({ navLinks }: Props) => {
-//   // Используем хук usePathname для получения текущего URL
-// //   const pathname = usePathname();
-
-// const {pathname} = useRouter()
-// // const navigation = [
-// //     {id: 1, title: "Home", patch: "/"},
-// //     {id: 2, title: "Posts", patch: "/posts"},
-// //     {id: 3, title: "About", patch: "/about"},
-// //     {id: 4, title: "Catalog", patch: "/catalog"}
-
-// // ]
-
-
-//   return (
-//     <div className={styles.container}>
-//       {/* Создаем навигационное меню с использованием Link */}
-//       <nav className={styles.nav}>
-//         {navLinks.map((link) => (
-            
-//           <Link key={link.href} href={link.href}>
-//             {/* Добавляем активное состояние для текущего URL*/}
-//             <a className={link.href === pathname ? styles.activeLink : styles.link}>
-
-//               {link.label}
-//             </a>
-//           </Link>
-       
-//         ))}
-//       </nav>
-//     </div>
-//   );
-// };
-// Определяем компонент навигации
 const Navigation = ({ navLinks }: Props) => {
-    const { pathname } = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <div className={styles.container}>
+      {navLinks.map((link) => {
+        const isActive = pathname === link.href;
+
+        return (
+          <Link
+            key={link.label}
+            href={link.href}
+            className={`${styles.link} ${isActive ? styles.activeLink : ""}`}
+          >
+            {link.label}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export { Navigation };
+
+
+
+
+
+
+// Импортируем необходимые модули и компоненты
+// "use client"; // Используется для всех клиентских компонентов
+// import styles from "./Navigation.module.css"; // Импортируем стили
+// import Link from "next/link";
+// import { usePathname } from "next/navigation";
+// // import {useRouter} from "next/router"
+
+// // Определяем тип для элемента навигации
+// type NavLink = {
+//   label: string;
+//   href: string;
+// };
+
+// // Определяем тип для пропсов компонента
+// type Props = {
+//   navLinks: NavLink[];
+// };
+
+// // Определяем компонент навигации
+// // const Navigation = ({ navLinks }: Props) => {
+// //   // Используем хук usePathname для получения текущего URL
+// // //   const pathname = usePathname();
+
+// // const {pathname} = useRouter()
+// // // const navigation = [
+// // //     {id: 1, title: "Home", patch: "/"},
+// // //     {id: 2, title: "Posts", patch: "/posts"},
+// // //     {id: 3, title: "About", patch: "/about"},
+// // //     {id: 4, title: "Catalog", patch: "/catalog"}
+
+// // // ]
+
+
+// //   return (
+// //     <div className={styles.container}>
+// //       {/* Создаем навигационное меню с использованием Link */}
+// //       <nav className={styles.nav}>
+// //         {navLinks.map((link) => (
+            
+// //           <Link key={link.href} href={link.href}>
+// //             {/* Добавляем активное состояние для текущего URL*/}
+// //             <a className={link.href === pathname ? styles.activeLink : styles.link}>
+
+// //               {link.label}
+// //             </a>
+// //           </Link>
+       
+// //         ))}
+// //       </nav>
+// //     </div>
+// //   );
+// // };
+// // Определяем компонент навигации
+// const Navigation = ({ navLinks }: Props) => {
+//   const pathname = usePathname();
   
-    return (
-      <div className={styles.container}>
-        {/* Создаем навигационное меню с использованием Link */}
-        <nav className={styles.nav}>
+//     return (
+//       <div className={styles.container}>
+//         {/* Создаем навигационное меню с использованием Link */}
+//         <nav className={styles.nav}>
          
-          {navLinks.map((link) => (
-            <Link key={link.href} href={link.href}>
-              {/* Добавляем активное состояние для текущего URL */}
-              <a className={pathname === link.href ? styles.activeLink : styles.link}>
-                {link.label}
-              </a>
-            </Link>
-          ))}
-        </nav>
-      </div>
-    );
-  };
+//           {navLinks.map((link) => (
+//             <Link key={link.href} href={link.href}>
+//               {/* Добавляем активное состояние для текущего URL */}
+//               <a className={pathname === link.href ? styles.activeLink : styles.link}>
+//                 {link.label}
+//               </a>
+//             </Link>
+//           ))}
+//         </nav>
+//       </div>
+//     );
+//   };
 
-export { Navigation }; // Экспортируем компонент навигации
-
-
-
-
-
+// export { Navigation }; // Экспортируем компонент навигации
--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -1,26 +1,60 @@
+// import Link from "next/link";
+// import styles from "./TheHeader.module.css";
+// import Image from "next/image"; // Импортируем компонент Image
+// import { Navigation } from "./Navigation";
+
+// const navItems = [
+//     {label: "Home", href: "/"},
+//     {label: "Posts", href: "/posts"},
+//     {label: "About", href: "/about"},
+//     {label: "Catalog", href: "/catalog"}]
+
+
+
+// const TheHeader = () => {
+//   return (
+//     <header className={styles.container}>
+//     <Navigation navLinks={navItems}/>
+
+//       <Link href="/" passHref>
+//         <span className={styles.link}>
+//           {/* Вставляем логотип с использованием компонента Image */}
+//           <Image src="/cat3.jpg" width={60} height={60} alt="logo" />
+//           Home
+//         </span>
+//       </Link>
+//       <Link href="/blog" passHref>
+//         <span className={styles.link}>Blog</span>
+//       </Link>
+//       <Link href="/about" passHref>
+//         <span className={styles.link}>About</span>
+//       </Link>
+//       <Link href="/catalog" passHref>
+//         <span className={styles.link}>Catalog</span>
+//       </Link>
+//     </header>
+//   );
+// };
+// TheHeader.tsx
+
 import Link from "next/link";
 import styles from "./TheHeader.module.css";
 import Image from "next/image"; // Импортируем компонент Image
 
+import { Navigation } from "./Navigation";
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/blog" },
+  { label: "About", href: "/about" },
+];
+
 const TheHeader = () => {
   return (
-    <header className={styles.container}>
-      <Link href="/" passHref>
-        <span className={styles.link}>
-          {/* Вставляем логотип с использованием компонента Image */}
-          <Image src="/cat3.jpg" width={60} height={60} alt="logo" />
-          Home
-        </span>
-      </Link>
-      <Link href="/blog" passHref>
-        <span className={styles.link}>Blog</span>
-      </Link>
-      <Link href="/about" passHref>
-        <span className={styles.link}>About</span>
-      </Link>
-      <Link href="/catalog" passHref>
-        <span className={styles.link}>Catalog</span>
-      </Link>
+    <header className={styles.header}>
+      <Image src="/cat3.jpg" width={60} height={60} alt="logo" className={styles.logo} />
+      <Navigation navLinks={navItems} />
+      
     </header>
   );
 };
