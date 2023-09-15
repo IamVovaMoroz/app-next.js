805f4eea12b3f881c746f159180e7b3fed428912
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-09-15 21:30:16 +0200

    ok

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -4,6 +4,9 @@ import {headers, cookies} from "next/headers"
 
 import {redirect} from "next/navigation"
 
+
+// http://localhost:3000/api/posts/2323   ид поста 2323 к примеру.
+
 // вторым можно указать res , но мы конкретезируем сразу {params}: {params: {id: string}}
 export async function DELETE(req: Request, {params}: {params: {id: string}}) {
     // считываем параметр с q
@@ -27,7 +30,7 @@ export async function DELETE(req: Request, {params}: {params: {id: string}}) {
 
 // import { NextResponse } from "next/server";
 
-// export async function DELETE(req, { params }) {
+// export async function DELETE(req: Request, {params}: {params: {id: string}}) {
 //     const id = params.id;
 //     // Удалить пост
 //     const message = `Пост с номером ${id} был удален`;
