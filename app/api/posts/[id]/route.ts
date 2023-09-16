1f5f148664ceb2bb8b932ea7dbe6539427ea0a91
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-09-16 22:27:40 +0200

    ok

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -5,7 +5,7 @@ import {headers, cookies} from "next/headers"
 import {redirect} from "next/navigation"
 
 
-// http://localhost:3000/api/posts/2323   ид поста 2323 к примеру.
+// http://localhost:3000/api/posts/2323   ид поста 2323 к примеру
 
 // вторым можно указать res , но мы конкретезируем сразу {params}: {params: {id: string}}
 export async function DELETE(req: Request, {params}: {params: {id: string}}) {
